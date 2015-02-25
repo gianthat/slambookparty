0f7ad9eef5742da48fc10d9dac7accb880c58549
Kathy <kheffern@gmail.com>
2015-02-25 11:30:50 -0600

    trying to move user editing to user controller, not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).concat [:first_name, :last_name]
-    devise_parameter_sanitizer.for(:account_update).concat [:first_name, :last_name, :pen_color]
+    devise_parameter_sanitizer.for(:sign_up).concat [:first_name, :last_name, :admin]
+    devise_parameter_sanitizer.for(:account_update).concat [:first_name, :last_name, :pen_color, :admin]
   end
 
   def after_sign_in_path_for(resource)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,37 @@
 class UsersController < ApplicationController
+	  before_action :set_user
 	  after_action :verify_authorized
 
 
-  def homeroom
-    @user = User.find(params[:id])
-    authorize @user
+  def homeroom 
   end
 
   def show
-    @user = User.find(params[:id])
-    authorize @user
   end
+
+  def edit
+	@pen_colors = User::PEN_COLORS
+  end
+
+  def update
+  	failure
+  	@pen_colors = User::PEN_COLORS
+      if @user.update(user_params)
+          redirect_to @user, notice: 'Update successful!'
+      else
+        render :edit, notice: "User was not updated"
+      end
+  end
+
+  private
+
+  	def set_user
+	  	@user = User.find(params[:id])
+  	  	authorize @user
+  	end
+
+  	def user_params
+  		params.require(:user).permit(:email, :first_name, :last_name, :admin, :pen_color,  :password, :password_confirmation)
+  	end
 
 end
